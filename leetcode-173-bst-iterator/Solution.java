85beb0b0309fffc6b97dc0851b70f63736463168
Andre Lobo <andre@dev-pro.ca>
2022-11-15 22:32:45 -0300

    should use static functions. Using example 1

--- leetcode-173-bst-iterator/Solution.java
+++ leetcode-173-bst-iterator/Solution.java
@@ -5,11 +5,29 @@ public class Solution{
         // in order: 4 -> 2 -> 5 -> 1 -> 6 -> 3 -> 7
         // pre order: 1 -> 2 -> 4 -> 5 -> 3 -> 6 -> 7
         // post order: 4 -> 5 -> 2 -> 6 -> 7 -> 3 -> 1
-        TreeNode root = new TreeNode(1,
-                new TreeNode(2, new TreeNode(4), new TreeNode(5)),
-                new TreeNode(3, new TreeNode(6), new TreeNode(7)));
+        if(args.length == 1 && args[0].equals("traverseOrders")){
+            TreeNode root = new TreeNode(1,
+                    new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                    new TreeNode(3, new TreeNode(6), new TreeNode(7)));
 
-        Solution.testTraversingOrders(root);
+            Solution.testTraversingOrders(root);
+            return ;
+        }
+
+        TreeNode root = new TreeNode(7,
+                new TreeNode(3),
+                new TreeNode(15, new TreeNode(9), new TreeNode(20)));
+
+        BSTIterator bSTIterator = new BSTIterator(root);
+        System.out.println(bSTIterator.next()); // return 3
+        System.out.println(bSTIterator.next()); // return 7
+        System.out.println(bSTIterator.hasNext()); // return True
+        System.out.println(bSTIterator.next()); // return 9
+        System.out.println(bSTIterator.hasNext()); // return True
+        System.out.println(bSTIterator.next()); // return 15
+        System.out.println(bSTIterator.hasNext()); // return True
+        System.out.println(bSTIterator.next()); // return 20
+        System.out.println(bSTIterator.hasNext()); // return False
 
     }
 
@@ -27,24 +45,24 @@ public class Solution{
     public static void printInOrder(TreeNode node){
         if(node == null) return ;
 
-        this.printInOrder(node.left);
+        Solution.printInOrder(node.left);
         System.out.printf("\n Node: %d", node.val);
-        this.printInOrder(node.right);
+        Solution.printInOrder(node.right);
     }
 
     public static void printPreOrder(TreeNode node){
         if(node == null) return ;
 
         System.out.printf("\n Node: %d", node.val);
-        this.printPreOrder(node.left);
-        this.printPreOrder(node.right);
+        Solution.printPreOrder(node.left);
+        Solution.printPreOrder(node.right);
     }
 
     public static void printPostOrder(TreeNode node){
         if(node == null) return ;
 
-        this.printPostOrder(node.left);
-        this.printPostOrder(node.right);
+        Solution.printPostOrder(node.left);
+        Solution.printPostOrder(node.right);
         System.out.printf("\n Node: %d", node.val);
     }
 }
