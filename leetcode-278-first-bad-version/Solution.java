b4fe6172a449d203ef0943cb760c580db6188521
Andre Lobo <andre@dev-pro.ca>
2022-11-20 12:15:51 -0300

    using binary search keep tracking of the last first bad version found. When the search ends with start == end, the first bad version is found as we are moving the start pointer right when the current version is not bad, and we are moving the end pointer left when the current version is bad. Worst case is O(logn) when the first bad version is 1.

---
+++ leetcode-278-first-bad-version/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Solution{
+//    Constraints:
+//    1 <= bad <= n <= 2^31 - 1
+    public static void main(String[] args){
+        if(Solution.firstBadVersion(5, new VersionControl(4)) == 4){
+            System.out.println("Ex1 correct");
+        }
+
+        if(Solution.firstBadVersion(1, new VersionControl(1)) == 1){
+            System.out.println("Ex2 correct");
+        }
+
+    }
+
+    // t: O(logn), s: O(1)
+    public static int firstBadVersion(int n, VersionControl api) {
+        int first = 0;
+
+        int start = 1, end = n, mid = -1;
+        while(start <= end){
+            mid = start + (end - start)/2;
+            if(api.isBadVersion(mid)){
+                first = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return first;
+    }
+
+}
