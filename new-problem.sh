4bf564daa2b4eb98f74b65e1a0f9f003706034aa
Andre Lobo <andre@dev-pro.ca>
2022-12-17 03:27:05 -0600

    created gradle project. Run it with `./gradlew run` so the file can be used to call the main classes from the respective problem we want.

--- new-problem.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git branch $1
-git checkout $1
-mkdir $1
-cd $1
-touch Solution.java
-echo "import java.util.*;public class Solution{public static void main(String[] args){}}" > Solution.java 
