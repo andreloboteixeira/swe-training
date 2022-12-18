d89763b27d424f66f7a8eee8fbf1b5465f4d383c
Andre Lobo <andre@dev-pro.ca>
2022-12-17 23:00:35 -0600

    Leetcode145ProblemBinaryTreePostorderTraversal

--- coding-problems-java/app/src/main/java/coding/problems/java/Leetcode145ProblemBinaryTreePostorderTraversal.java
+++ coding-problems-java/app/src/main/java/coding/problems/java/Leetcode145ProblemBinaryTreePostorderTraversal.java
@@ -1,5 +1,7 @@
+package coding.problems.java;
+
 import java.util.*;
-public class Solution{
+public class Leetcode145ProblemBinaryTreePostorderTraversal{
     public static void main(String[] args){
         TreeNode ex1Root = new TreeNode(1,
                 null, new TreeNode(2,
@@ -7,25 +9,25 @@ public class Solution{
 
 
         System.out.println("Ex1 my solution");
-        for(Integer val: Solution.postorderTraversal(ex1Root))
+        for(Integer val: Leetcode145ProblemBinaryTreePostorderTraversal.postorderTraversal(ex1Root))
             System.out.printf("%d, ", val);
 
 
         TreeNode ex2Root = null;
         System.out.println("\n\nEx2 my solution");
-        for(Integer val: Solution.postorderTraversal(ex2Root))
+        for(Integer val: Leetcode145ProblemBinaryTreePostorderTraversal.postorderTraversal(ex2Root))
             System.out.printf("%d, ", val);
 
         TreeNode ex3Root = new TreeNode(1);
         System.out.println("\n\nEx3 my solution");
-        for(Integer val: Solution.postorderTraversal(ex3Root))
+        for(Integer val: Leetcode145ProblemBinaryTreePostorderTraversal.postorderTraversal(ex3Root))
             System.out.printf("%d, ", val);
 
         TreeNode ex4Root = new TreeNode(1,
                 new TreeNode(2, new TreeNode(3), new TreeNode(4)),
                 new TreeNode(5, new TreeNode(6), new TreeNode(7)));
         System.out.println("\n\nEx4 my solution");
-        for(Integer val: Solution.postorderTraversal(ex4Root))
+        for(Integer val: Leetcode145ProblemBinaryTreePostorderTraversal.postorderTraversal(ex4Root))
             System.out.printf("%d, ", val);
 
     }
@@ -34,15 +36,15 @@ public class Solution{
         List<Integer> postOrder = new ArrayList<>();
         if(root == null) return postOrder;
 
-        Solution.helper(root, postOrder);
+        Leetcode145ProblemBinaryTreePostorderTraversal.helper(root, postOrder);
         return postOrder;
     }
 
     public static void helper(TreeNode node, List<Integer> postOrder){
         if(node == null) return ;
 
-        Solution.helper(node.left, postOrder);
-        Solution.helper(node.right, postOrder);
+        Leetcode145ProblemBinaryTreePostorderTraversal.helper(node.left, postOrder);
+        Leetcode145ProblemBinaryTreePostorderTraversal.helper(node.right, postOrder);
         postOrder.add(node.val);
     }
 }
