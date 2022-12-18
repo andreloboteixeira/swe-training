73222641cf096cc1b5eb8be294b08928903d519d
Andre Lobo <andre@dev-pro.ca>
2022-12-17 22:42:53 -0600

    Leetcode53MaximumSubarray

--- coding-problems-java/app/src/main/java/coding/problems/java/Leetcode53MaximumSubarray.java
+++ coding-problems-java/app/src/main/java/coding/problems/java/Leetcode53MaximumSubarray.java
@@ -1,15 +1,17 @@
+package coding.problems.java;
+
 import java.util.*;
-public class Solution{
+public class Leetcode53MaximumSubarray{
     public static void main(String[] args){
-        if(Solution.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}) == 6){
+        if(Leetcode53MaximumSubarray.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}) == 6){
             System.out.println("Ex1 correct");
         }
 
-        if(Solution.maxSubArray(new int[]{1}) == 1){
+        if(Leetcode53MaximumSubarray.maxSubArray(new int[]{1}) == 1){
             System.out.println("Ex2 correct");
         }
 
-        if(Solution.maxSubArray(new int[]{5,4,-1,7,8}) == 23){
+        if(Leetcode53MaximumSubarray.maxSubArray(new int[]{5,4,-1,7,8}) == 23){
             System.out.println("Ex3 correct");
         }
 
