ebfe54e3a905a517e29808d8af9f0a0cf862d305
Andre Lobo <andre@dev-pro.ca>
2022-12-17 22:54:09 -0600

    Leetcode105ProblemConstructBSTFromPreorderAndInorder

--- coding-problems-java/app/src/main/java/coding/problems/java/Leetcode105ProblemConstructBSTFromPreorderAndInorder.java
+++ coding-problems-java/app/src/main/java/coding/problems/java/Leetcode105ProblemConstructBSTFromPreorderAndInorder.java
@@ -1,38 +1,40 @@
+package coding.problems.java;
+
 import java.util.*;
-public class Solution{
+public class Leetcode105ProblemConstructBSTFromPreorderAndInorder{
     public static void main(String[] args){
-        TreeNode resEx1 = Solution.buildTree(new int[]{3,9,20,15,7}, new int[]{9,3,15,20,7});
-        Solution.printTree(resEx1);
+        TreeNode resEx1 = Leetcode105ProblemConstructBSTFromPreorderAndInorder.buildTree(new int[]{3,9,20,15,7}, new int[]{9,3,15,20,7});
+        Leetcode105ProblemConstructBSTFromPreorderAndInorder.printTree(resEx1);
     }
 
     public static void printTree(TreeNode root){
         System.out.println("Pre-order");
-        Solution.printPreorder(root);
+        Leetcode105ProblemConstructBSTFromPreorderAndInorder.printPreorder(root);
         System.out.println("In-order");
-        Solution.printInorder(root);
+        Leetcode105ProblemConstructBSTFromPreorderAndInorder.printInorder(root);
     }
 
     public static void printPreorder(TreeNode node){
         if(node == null) return ;
 
         System.out.println(node.val);
-        Solution.printPreorder(node.left);
-        Solution.printPreorder(node.right);
+        Leetcode105ProblemConstructBSTFromPreorderAndInorder.printPreorder(node.left);
+        Leetcode105ProblemConstructBSTFromPreorderAndInorder.printPreorder(node.right);
     }
 
     public static void printInorder(TreeNode node){
         if(node == null) return ;
 
-        Solution.printInorder(node.left);
+        Leetcode105ProblemConstructBSTFromPreorderAndInorder.printInorder(node.left);
         System.out.println(node.val);
-        Solution.printInorder(node.right);
+        Leetcode105ProblemConstructBSTFromPreorderAndInorder.printInorder(node.right);
     }
 
     // t: O(n), a function call for every element, function is constant using the inorderMap
     // s: O(n), using the map to save time that would be O(n^2) if a linear search in the helper function was used instead
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> inorderMap = Solution.buildInorderMap(inorder);
-        TreeNode root = Solution.helper(preorder, inorderMap, 0, 0, inorder.length - 1);
+        Map<Integer, Integer> inorderMap = Leetcode105ProblemConstructBSTFromPreorderAndInorder.buildInorderMap(inorder);
+        TreeNode root = Leetcode105ProblemConstructBSTFromPreorderAndInorder.helper(preorder, inorderMap, 0, 0, inorder.length - 1);
         return root;
     }
 
@@ -57,11 +59,11 @@ public class Solution{
 
         // the root node at the left will be created with the next element in the preorder (preorder++)
         // and the left tree found in the inorder array
-        newRoot.left = Solution.helper(preorder, inorderMap, preStart + 1, inStart, mid - 1);
+        newRoot.left = Leetcode105ProblemConstructBSTFromPreorderAndInorder.helper(preorder, inorderMap, preStart + 1, inStart, mid - 1);
         // the root node at the right should be created with the first righ element in preorder,
         // that is, after all left tree elements (preorder + mid - inStart + 1), (mid - inStart) shifts the left tree
         // and the right tree found in the inorder array
-        newRoot.right = Solution.helper(preorder, inorderMap, preStart + mid - inStart + 1, mid + 1, inEnd);
+        newRoot.right = Leetcode105ProblemConstructBSTFromPreorderAndInorder.helper(preorder, inorderMap, preStart + mid - inStart + 1, mid + 1, inEnd);
         return newRoot;
 
     }
